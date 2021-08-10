20f74093febb6eb69d3618005007dbc9cf9b0300
D. Eugene <evgdob@gmail.com>
2021-08-10 22:17:10 +0300

    first commit

---
+++ NugetUpgrade/NugetUpgradePackage.cs
@@ -0,0 +1,47 @@
+﻿using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace NugetUpgrade
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(NugetUpgradePackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class NugetUpgradePackage : AsyncPackage
+    {
+        public const string PackageGuidString = "850c3a4a-c1a5-4641-91e4-2fe47a87427c";
+
+        public static NugetUpgradePackage Instance;
+        internal DTE2 _dte;
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            Instance = this;
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await UpgradePackages.InitializeAsync(this);
+
+
+        }
+
+    }
+}
---
+++ NugetUpgrade/UpgradePackages.cs
@@ -0,0 +1,185 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Task = System.Threading.Tasks.Task;
+
+namespace NugetUpgrade
+{
+
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class UpgradePackages
+    {
+        public const int CommandId = 0x0100;
+        public static readonly Guid CommandSet = new Guid("1cbe85f2-1989-4d41-a09b-4d42b7068d92");
+        private readonly AsyncPackage package;
+        private static bool _isProcessing;
+        public static DTE2 _dte;
+
+        
+        public static UpgradePackages Instance;
+       
+
+        private UpgradePackages(AsyncPackage package, OleMenuCommandService commandService, DTE2 dte)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+            _dte = dte;
+        }
+
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {          
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            DTE2 dte = await package.GetServiceAsync(typeof(DTE)) as DTE2;
+            Instance = new UpgradePackages(package, commandService, dte);
+            await Logger.InitializeAsync(package);
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            UpgradePackagesConfig();
+        }
+
+
+        private void UpgradePackagesConfig()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            _isProcessing = true;
+
+            var projects = ProjectHelpers.GetAllProjects().Where(c => File.Exists(c.GetFullPath()+ @"\packages.config"));
+
+            if (!projects.Any())
+            {
+                _dte.StatusBar.Text = "Please select a package.config file to nuke from orbit.";
+                _isProcessing = false;
+                return;
+            }
+
+            //var projectFolder = ProjectHelpers.GetRootFolder(ProjectHelpers.GetActiveProject());
+            int count = projects.Count();
+
+            //RWM: Don't mess with these.
+            XNamespace defaultNs = "http://schemas.microsoft.com/developer/msbuild/2003";
+            var options = new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount };
+
+            try
+            {
+                string text = count == 1 ? " file" : " files";
+                _dte.StatusBar.Progress(true, $"Fixing {count} config {text}...", AmountCompleted: 1, Total: count + 1);
+
+                Parallel.For(0, count, options, i =>
+                {
+                    var packageReferences = new XElement(defaultNs + "ItemGroup");
+                    var packagesConfigItem = projects.ElementAt(i);
+                    var packagesConfigPath = packagesConfigItem.GetFullPath() + @"\packages.config";
+                    var projectPath = packagesConfigItem.FileName;
+
+                    //RWM: Start by backing up the files.
+                    File.Copy(packagesConfigPath, $"{packagesConfigPath}.bak", true);
+                    File.Copy(projectPath, $"{projectPath}.bak", true);
+
+                   // Logger.Instance.LogAsync($"Backup created for {packagesConfigPath}.").Wait();
+
+                    //RWM: Load the files.
+                    var project = XDocument.Load(projectPath);
+                    var packagesConfig = XDocument.Load(packagesConfigPath);
+
+                    //RWM: Get references to the stuff we're gonna get rid of.
+                    var oldReferences = project.Root.Descendants().Where(c => c.Name.LocalName == "Reference");
+                    var errors = project.Root.Descendants().Where(c => c.Name.LocalName == "Error");
+                    var targets = project.Root.Descendants().Where(c => c.Name.LocalName == "Import");
+
+                    foreach (var row in packagesConfig.Root.Elements().ToList())
+                    {
+                        //RWM: Create the new PackageReference.
+                        packageReferences.Add(new XElement(defaultNs + "PackageReference",
+                            new XAttribute("Include", row.Attribute("id").Value),
+                            new XAttribute("Version", row.Attribute("version").Value)));
+
+                        //RWM: Remove the old Standard Reference.
+                        oldReferences.Where(c => c.Attribute("Include").Value.Split(new Char[] { ',' })[0].ToLower() == row.Attribute("id").Value.ToLower()).ToList()
+                            .ForEach(c => c.Remove());
+                        //RWM: Remove any remaining Standard References where the PackageId is in the HintPath.
+                        oldReferences.Where(c => c.Descendants().Any(d => d.Value.Contains(row.Attribute("id").Value))).ToList()
+                            .ForEach(c => c.Remove());
+                        //RWM: Remove any Error conditions for missing Package Targets.
+                        errors.Where(c => c.Attribute("Condition").Value.Contains(row.Attribute("id").Value)).ToList()
+                            .ForEach(c => c.Remove());
+                        //RWM: Remove any Package Targets.
+                        targets.Where(c => c.Attribute("Project").Value.Contains(row.Attribute("id").Value)).ToList()
+                            .ForEach(c => c.Remove());
+                    }
+
+                    //RWM: Fix up the project file by adding PackageReferences, removing packages.config, and pulling NuGet-added Targets.
+                    project.Root.Elements().First(c => c.Name.LocalName == "ItemGroup").AddBeforeSelf(packageReferences);
+                    var packageConfigReference = project.Root.Descendants().FirstOrDefault(c => c.Name.LocalName == "None" && c.Attribute("Include").Value == "packages.config");
+                    if (packageConfigReference != null)
+                    {
+                        packageConfigReference.Remove();
+                    }
+
+                    var nugetBuildImports = project.Root.Descendants().FirstOrDefault(c => c.Name.LocalName == "Target" && c.Attribute("Name").Value == "EnsureNuGetPackageBuildImports");
+                    if (nugetBuildImports != null && nugetBuildImports.Descendants().Count(c => c.Name.LocalName == "Error") == 0)
+                    {
+                        nugetBuildImports.Remove();
+                    }
+
+                    //RWM: Upgrade the ToolsVersion so it can't be opened in VS2015 anymore.
+                    project.Root.Attribute("ToolsVersion").Value = "15.0";
+
+                    //RWM: Save the project and delete Packages.config.
+                    ProjectHelpers.CheckFileOutOfSourceControl(projectPath);
+                    ProjectHelpers.CheckFileOutOfSourceControl(packagesConfigPath);
+                    project.Save(projectPath, SaveOptions.None);
+                    File.Delete(packagesConfigPath);
+
+                 //   Logger.Instance.LogAsync($"Update complete. Visual Studio will prompt you to reload the project now.").Wait();
+                });
+            }
+            catch (AggregateException agEx)
+            {
+                _dte.StatusBar.Progress(false);
+               // Logger.Instance.LogAsync($"Update failed. Exceptions:").Wait();
+                //foreach (var ex in agEx.InnerExceptions)
+                //{
+                ////    Logger.Instance.LogAsync($"Message: {ex.Message}{Environment.NewLine}{ex.StackTrace}").Wait();
+                //}
+                _dte.StatusBar.Text = "Operation failed. Please see Output Window for details.";
+                _isProcessing = false;
+
+            }
+            finally
+            {
+                _dte.StatusBar.Progress(false);
+                _dte.StatusBar.Text = "Operation finished. Please see Output Window for details.";
+                _isProcessing = false;
+            }
+
+        }
+    }
+}
